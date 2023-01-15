0db203cb7652470d1dee266ccf1de957aaeeaf61
fahrenq <git@ivan.engineering>
2023-01-15 19:28:09 +0100

    Fix button placement

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -14,18 +14,18 @@ export default function Result(
         <div class="text-center text-3xl">Resultat</div>
       </div>
 
+      <div className="mt-16 flex flex-col items-center gap-2">
+        <div
+          className="p-2 bg-white/[.9] rounded w-48 text-center border border-amber-300 border-4 cursor-pointer hover:border-amber-500 transition select-none"
+          onClick={onMainMenu}>
+          Hovedmenu
+        </div>
+      </div>
+
       <div class="mt-20 bg-white/[.7] text-xl text-center">
         I {timeLimit} sekunder, fik du {stats.currentScore} ud af {stats.totalWords} rigtige.
         <br/>
         Din bedste streak var {stats.bestStreak}.
-      </div>
-
-      <div class="mt-28 flex flex-col items-center gap-2">
-        <div
-          class="p-2 bg-white/[.9] rounded w-48 text-center border border-amber-300 border-4 cursor-pointer hover:border-amber-500 transition select-none"
-          onClick={onMainMenu}>
-          Hovedmenu
-        </div>
       </div>
     </div>
   )
